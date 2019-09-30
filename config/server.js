3b4f02f837b49e41f3837ff1f9dedfedc372d997
Yassin Bahassane <yassinfreethinker@gmail.com>
2019-09-30 11:57:37 +0200

    Infinite Scrolling Challenge

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || "55906b444bdc348c930f415b8ece6d7ad05b821b7d530142cc5a47c790d1372f",
+    SECRET: process.env.SECRET || "1f7e2ddd8f751b03371ba06fe75e6bb96bff437e15eb87d50f4925dcca8bde0b",
+    CALLBACK_URL: process.env.CALLBACK_URL || "http://localhost:3000"
+}
